08603cf55a024a0ae77f10a22c6f1057efb18be6
ShengYan, Zhang <yan.code@gmail.com>
2023-03-22 09:50:50 +0800

    fix: temp disable mailer send as the server is not working

--- auth-api/src/utils/mailer.ts
+++ auth-api/src/utils/mailer.ts
@@ -1,5 +1,5 @@
-import nodemailer, { SendMailOptions } from 'nodemailer';
-import config from 'config';
+import { SendMailOptions } from 'nodemailer';
+// import config from 'config';
 import { log } from './logger';
 
 // async function createTestCreds() {
@@ -7,23 +7,25 @@ import { log } from './logger';
 //   console.log('creds', creds);
 // }
 // createTestCreds();
-const smtp = config.get<{
-  user: string;
-  pass: string;
-  host: string;
-  port: number;
-  secure: boolean;
-}>('smtp');
-const transporter = nodemailer.createTransport({
-  ...smtp,
-  auth: { user: smtp.user, pass: smtp.pass },
-});
+// const smtp = config.get<{
+//   user: string;
+//   pass: string;
+//   host: string;
+//   port: number;
+//   secure: boolean;
+// }>('smtp');
+// const transporter = nodemailer.createTransport({
+//   ...smtp,
+//   auth: { user: smtp.user, pass: smtp.pass },
+// });
 
 export async function sendEmail(payload: SendMailOptions) {
-  transporter.sendMail(payload, (err, info) => {
-    if (err) {
-      return log.error(err, 'Error sending email');
-    }
-    log.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
-  });
+  log.info(`sending email ${payload.text}`);
+  //todo enable bellow send mail function when smtp.ethereal.email recovers
+  // transporter.sendMail(payload, (err, info) => {
+  //   if (err) {
+  //     return log.error(err, 'Error sending email');
+  //   }
+  //   log.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+  // });
 }
